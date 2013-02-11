92d67331152b1860655e0e34f511fc37481c1e85
HarHar <harhar-captain@live.com>
2013-02-11 22:20:22 +0000

    Change admin port to 6666 because fuck you

--- core/engine_admin.py
+++ core/engine_admin.py
@@ -134,6 +134,6 @@ def start(botInstance, modules):
 	for module in moduleInstances:
 		moduleInstances[module]['instance'].setAdmins(admin_clients)
 
-	print '[\033[94minfo\033[0m] Listening on 6667 for administrators'
-	server = SocketServer.ThreadingTCPServer(('', 6667), SEngine)
+	print '[\033[94minfo\033[0m] Listening on 6666 (irc) for administrators'
+	server = SocketServer.ThreadingTCPServer(('', 6666), SEngine)
 	server.serve_forever()
