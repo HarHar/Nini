227cdfeba1ba3168bf86b25038f9bf18d8e1a3ce
HarHar <harhar-captain@live.com>
2013-02-11 21:49:34 +0000

    Forgot to add persistent variable module

---
+++ var/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+import sys
+import pickle
+
+#Persistent variables
+class persVars(object):
+	def __init__(self):
+		self.vardir = os.path.dirname(os.path.realpath(sys.argv[0]))
+	def __getitem__(self, item):
+		if isinstance(item, str) == False:
+			print '[NON-FATAL] ' + repr(item) + ' is not a string'
+			return None
+
+		varfile = os.path.join(self.vardir + item + '.var')
+		if os.path.exists(varfile):
+			return pickle.load(open(varfile, 'r'))
+		else:
+			return {}
+	def __setitem__(self, item, content):
+		if isinstance(item, str) == False:
+			print '[NON-FATAL] ' + repr(item) + ' is not a string'
+			return None
+
+		varfile = os.path.join(self.vardir + item + '.var')
+		pickle.dump(content, open(varfile, 'w'))
