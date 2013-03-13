2d503e2bfee629d5c4da284e7e0012a3829fafb4
HarHar <harhar-captain@live.com>
2013-03-12 21:04:35 -0300

    Add 8ball

---
+++ modules/8ball.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import random
+
+class BotModule(object):
+	def __init__(self, storage):
+		self.storage = storage
+		self.answers = [chr(3) + '04Of course not!', chr(3) + '05No', 'Mayhaps', chr(3) + '03Yes', chr(3) + '02Sure!']
+	def register(self):
+		return {'functions': [{'8ball': self.cmd_8ball}]}
+	def cmd_8ball(self, args, receiver, sender, sender_address):
+		"""test2 [question] | True | Ping-pong"""
+		if receiver[0] == '#':
+			self.msg(receiver, chr(2) + 'Question: ' + chr(15) + args)
+			self.msg(receiver, chr(2) + 'Answer: ' + chr(15) + random.choice(self.answers))
