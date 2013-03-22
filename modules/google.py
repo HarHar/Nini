6b188c48cd0122099e1e5da9a7d8587ae495f5ab
TacticalGenius <abtalerico@gmail.com>
2013-03-22 16:54:25 -0500

    added youtube search

--- modules/google.py
+++ modules/google.py
@@ -10,24 +10,24 @@ class BotModule(object):
         self.storage = storage
         self.admins = {}
         self.bot = None
-        self.apiKey = 'AIzaSyBCJciReska7RcTlpOLen7rSrxXmSOVBA4'
-        self.gbooksQueryUrl = 'https://www.googleapis.com/books/v1/volumes?q='
-    
+        self.apiKey = "AIzaSyBCJciReska7RcTlpOLen7rSrxXmSOVBA4"
+        self.gbooksQueryUrl = "https://www.googleapis.com/books/v1/volumes?q="
+        self.ytQueryUrl = "http://gdata.youtube.com/feeds/api/videos?alt=json&q="
+
     def register(self):
-        return {'functions': [{'gbooks': self.gbooks}, {'shorten': self.shorten}]}
+        return {'functions': [{'gbooks': self.gbooks}, {'shorten': self.shorten}, {'youtube': self.youtube}]}
 
     def gbooks(self, args, receiver, sender):
-        """ Searchs Google Books for the arguement and displays info about
-            the first result. """
+        """ Searchs Google Books for the arguement and displays info about the first result. """
         if args == "$next":
-            self.resultNum = self.resultNum + 1
-            result = self.results['items'][self.resultNum]
+            self.gbResultNum = self.gbResultNum + 1
+            result = self.gbResults['items'][self.gbResultNum]
         else:
-            self.resultNum = 0
+            self.gbResultNum = 0
             args = args.replace(' ', '+')
             args = quote(args)
-            self.results = json.load(urlopen(self.gbooksQueryUrl + args + '&key=' + self.apiKey))
-            result = self.results['items'][0]
+            self.gbResults = json.load(urlopen(self.gbooksQueryUrl + args + '&key=' + self.apiKey))
+            result = self.gbResults['items'][0]
         title = result['volumeInfo']['title']
         if "authors" not in result['volumeInfo']:
             author = "Unavailable"
@@ -51,6 +51,24 @@ class BotModule(object):
         """ Takes a url and provides a shortened goo.gl link. """
         link = self.shortenUrl(args)
         self.bot.msg(reciever.name, sender.nick + ': ' + link)
+
+    def youtube(self, args, receiver, sender):
+        """ Searchs Google Books for the arguement and displays info about
+            the first result. """
+        if args == "$next":
+            self.ytResultNum = self.ytResultNum + 1
+            result = self.ytResults[self.ytResultNum]
+        else:
+            self.ytResultNum = 0
+            args = args.replace(' ', '+')
+            args = quote(args)
+            self.ytResults = json.load(urlopen(self.ytQueryUrl + args + '&key=' + self.apiKey))["feed"]["entry"]
+            result = self.ytResults[0]
+        title = result["title"]["$t"].encode("ascii", "ignore")
+        link = self.shortenUrl(result["link"][0]["href"])
+        self.bot.msg(receiver.name, chr(2) + u'Title: ' + chr(15) + title + chr(2) + ' Link: ' + chr(15) + link)
+        if self.ytResultNum == 0:
+            self.bot.msg(receiver.name, "For the next result use '@youtube $next'")
 
     def shortenUrl(self, url):
         data = json.dumps({'longUrl': url})
