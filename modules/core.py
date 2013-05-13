d3b35dc1e6e24d10890b8f3d0b4fe381002f7559
HarHar <harhar-captain@live.com>
2013-05-13 18:20:21 -0300

    Add required doc to restart and quit functions

--- modules/core.py
+++ modules/core.py
@@ -30,10 +30,12 @@ class BotModule(object):
 			sender.msg('I am hosting my command list at ' + chr(2) + URL)
 
 	def quit(self, args, receiver, sender):
+		"""quit | False | quits bot """
 		self.bot.msg(receiver.name, "Exiting on user command")
 		self.bot.quit()
 
 	def restart(self, args, receiver, sender):
+		"""quit | False | restarts bot """
 		self.bot.msg(receiver.name, "IMPLEMENT ME")
 
 	def http(self, path, handler):
