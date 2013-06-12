b51eff7355875dbe993f26f53a6be34a2045e789
TacticalGenius <abtalerico@gmail.com>
2013-06-11 20:25:58 -0500

    added note about dependencies to module fetching script

--- modules/getExtraModules.sh
+++ modules/getExtraModules.sh
@@ -2,3 +2,4 @@
 git clone git://github.com/HarHar/NiNiModules.git
 mv NiNiModules/*.py .
 rm -f -R NiNiModules
+echo "Note: Addition dependcies will probably need to be installed through pip to load all modules"
